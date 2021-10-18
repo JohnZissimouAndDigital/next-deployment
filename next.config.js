bd176fc714e3bedb28c6b68be198354e4b1d6d8a
John Zissimou <john.zissimou@and.digital>
2021-10-18 12:34:21 +0100

    Added the enviroment variables in next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    OKTA_CLIENTID: process.env.OKTA_CLIENTID,
+    OKTA_CLIENTSECRET: process.env.OKTA_CLIENTSECRET,
+    OKTA_DOMAIN: process.env.OKTA_DOMAIN,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL
+  }
 }
